092268e2e4b4be7652b3e9db77a245285a553c76
Eric Holton <eric.holton@gmail.com>
2024-04-30 13:00:03 -0600

    fix netlify site id

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
   .Factory
   .CreateWeb(args)
   .DeployToNetlify(
-      "peaceful-keller-9f51f6",
+      "47e16c00-c0cf-41db-a118-1b96a54c3d79",
       Config.FromSetting<string>("NetlifyAccessToken")
   )  
   .RunAsync();
